67b9ccf3fa739989f9444328a445f2bb7cf7e5a5
Priyanshi Sahu <priyanshi.sahu@iicstechnologies.com>
2024-11-01 17:42:07 +0530

    first commit

---
+++ src/main/java/com/alibou/batch/config/StudentProcessor.java
@@ -0,0 +1,13 @@
+package com.alibou.batch.config;
+
+import com.alibou.batch.student.Student;
+import org.springframework.batch.item.ItemProcessor;
+
+public class StudentProcessor implements ItemProcessor<Student,Student> {
+
+    @Override
+    public Student process(Student student) {
+        student.setId(null);
+        return student;
+    }
+}
